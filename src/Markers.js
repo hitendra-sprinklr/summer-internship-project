110bc0d5f267d8f062619ad70b494c1f3059a228
hitendra-sprinklr <hitendra.singh@sprinklr.com>
2022-06-10 17:43:02 +0530

    added popup to markers

--- src/Markers.js
+++ src/Markers.js
@@ -1,4 +1,4 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Markers = () => {
   return (
@@ -8,10 +8,18 @@ const Markers = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}></Marker>
-        <Marker position={[51.51, -0.1]}></Marker>
-        <Marker position={[51.51, -0.12]}></Marker>
-        <Marker position={[51.52, -0.12]}></Marker>
+        <Marker position={[51.505, -0.09]}>
+          <Popup>This is a Popup!</Popup>
+        </Marker>
+        <Marker position={[51.51, -0.1]}>
+          <Popup>This is a Popup!</Popup>
+        </Marker>
+        <Marker position={[51.51, -0.12]}>
+          <Popup>This is a Popup!</Popup>
+        </Marker>
+        <Marker position={[51.52, -0.12]}>
+          <Popup>This is a Popup!</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
